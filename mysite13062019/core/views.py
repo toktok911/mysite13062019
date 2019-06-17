b48e1d9791a4933b613063245441f697a2e4497f
warrag <test@test.org>
2019-06-17 12:55:44 +0300

    my first commit

---
+++ mysite13062019/core/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+
+def home(request):
+	return render(request, 'home.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {
+        'form': form
+    })
+
+@login_required 
+def secret_page(request):
+	return render(request, 'secret_page.html')
